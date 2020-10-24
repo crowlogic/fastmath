586bf4d7a58fdd2a6bfdf8cf2f221812a8c6d6a1
SC <crow@localhost>
2020-10-24 16:37:53 -0500

    ...

---
+++ src/fastmath/Complex.java
@@ -0,0 +1,31 @@
+package fastmath;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.Structure;
+
+public class Complex extends
+                     Structure
+{
+
+  public double realPart;
+  public double imaginaryPart;
+
+  @Override
+  protected List<String> getFieldOrder()
+  {
+    return Arrays.asList("realPart", "imaginaryPart");
+  }
+
+  public Complex()
+  {
+  }
+
+  public Complex(Pointer p)
+  {
+    super(p);
+  }
+
+}
