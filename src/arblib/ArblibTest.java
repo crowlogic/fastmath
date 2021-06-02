65820656635f5bb4df3470fb3d505d8c17d23a92
SC <crow@localhost>
2021-06-01 23:44:16 -0500

    werkin

--- src/arblib/ArblibTest.java
+++ src/arblib/ArblibTest.java
@@ -7,13 +7,25 @@ public class ArblibTest extends
 {
   public void testHardyZFunction()
   {
+    System.loadLibrary("arblib");
+
     acb_struct result = new acb_struct();
     acb_struct input = new acb_struct();
     arblib.acb_set_d(input, 13.2);
-    System.out.println("Input");
+    System.err.println("Input");
     arblib.acb_print(input);
+    System.err.println();
+    System.err.flush();
+    // TODO: check that this is equal to ((3715469692580659 * 2^-48) +/- (0), (0)
+    // +/- (0))=13.2
+
     arblib.acb_dirichlet_hardy_z(result, input, null, null, 1, 20);
-    System.out.println("Result");
+    System.err.println("Result");
     arblib.acb_print(result);
+    System.err.println();
+    System.err.flush();
+    // TODO: check that this is equal to ((-351261 * 2^-19) +/- (690458335 * 2^-45),
+    // (0) +/- (0))=-.6699780149
+
   }
 }
