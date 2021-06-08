ef7f2666b08f31c3100f59379572a8c9883bbb11
SC <crow@localhost>
2021-06-07 20:17:28 -0500

    theta(13.2)=-2.091677832510246 e^(-2.091677832510246 + I0.0)= -0.497644932260519 + I-0.8673808398825877 should be  -.497644932260514 + I-0.867380839882591

--- src/arblib/ArblibTest.java
+++ src/arblib/ArblibTest.java
@@ -4,6 +4,7 @@ import static arblib.Constants.*;
 import static arblib.arblib.*;
 import static java.lang.Math.floor;
 import static java.lang.Math.min;
+import static java.lang.Math.pow;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -32,7 +33,7 @@ public class ArblibTest extends
     acb_set_d(input, 13.2);
 
     acb_dirichlet_hardy_z(result, input, null, null, 1, prec);
-    double res = point(result);
+    double res = point(result.getReal());
     assertEquals(-0.6699771881103516, res, 0.000001);
 
     // TODO: check that this is equal to ((-351261 * 2^-19) +/- (690458335 * 2^-45),
@@ -49,15 +50,28 @@ public class ArblibTest extends
 
     acb_set_d(z, 13.2);
     acb_dirichlet_hardy_theta(faze, z, null, null, 1, prec);
-    double result = point(faze);
-    out.println("theta(" + point(z) + ")=" + result );
-    assertEquals( -2.0916778325102459547, result );
+    double result = point(faze.getReal());
+    out.println("theta(" + point(z.getReal()) + ")=" + result);
+    assertEquals(-2.0916778325102459547, result);
+    out.println("e^(" + point(faze) + ")=");
+    faze.setImag(faze.getReal());
+    arb_set_d(faze.getReal(), 0);
+    acb_exp(faze, faze, prec);
+    out.println(point(faze) + " should be\n -.497644932260514 + I-0.867380839882591 ");
+    assertEquals( -0.497644932260514, point( faze.getReal() ), pow(10,-12) );
+    assertEquals( -0.867380839882591, point( faze.getImag() ), pow(10,-12) );
+    // -.497644932260514-.867380839882591*I
+
   }
 
-  private double point(acb_struct faze)
+  public static String point(acb_struct faze)
   {
-    return arf_get_d(faze.getReal().getMid(), prec );
+    return point(faze.getReal()) + " + I" + point(faze.getImag());
   }
 
+  public static double point(arb_struct faze)
+  {
+    return arf_get_d(faze.getMid(), prec);
+  }
 
 }
